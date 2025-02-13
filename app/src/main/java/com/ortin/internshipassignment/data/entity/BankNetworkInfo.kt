da282d812f486d47d8cf7baca72ff4b38226c688
Nikita Savvin <nik.savvin.2004@mail.ru>
2025-02-13 22:56:32 +0300

    Change version of ksp and update field name in BankNetworkInfo

--- app/src/main/java/com/ortin/internshipassignment/data/entity/BankNetworkInfo.kt
+++ app/src/main/java/com/ortin/internshipassignment/data/entity/BankNetworkInfo.kt
@@ -1,7 +1,7 @@
 package com.ortin.internshipassignment.data.entity
 
 data class BankNetworkInfo(
-    val name: String?,
+    val bankName: String?,
     val url: String?,
     val phone: String?,
     val city: String?
